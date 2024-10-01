6ff38f29d265ffa82574cc4c47e21b64bf30bc5e
MrJeleika <den932005@gmail.com>
2024-10-01 23:06:28 +0300

    feat: amazing inpit

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,20 +3,27 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  variant?: 'transparent' | 'filled';
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, variant = 'filled', ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          'pixel-corners flex h-10 w-full bg-blue-500 px-3 py-2 text-sm shadow-[-3px_0_0_0_#121212,3px_0_0_0_#121212,0_-3px_0_0_#121212,0_3px_0_0_#121212] file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[inset_-3px_0_0_0_#F9F6EE,inset_3px_0_0_0_#F9F6EE,inset_0_-3px_0_0_#F9F6EE,inset_0_3px_0_0_#F9F6EE]',
-          className,
-        )}
-        ref={ref}
-        {...props}
-      />
+      <div className="pixel-corners-wrapper w-full bg-primary p-[3px]">
+        <input
+          type={type}
+          className={cn(
+            'pixel-corners flex h-10 w-full px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50',
+            variant === 'filled'
+              ? 'bg-transparent caret-background'
+              : 'bg-background',
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
     );
   },
 );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -72,6 +72,7 @@ export default function Home() {
           value={amountOfSol}
           onChange={(e) => setAmountOfSol(e.target.value)}
           placeholder="Amount of SOL"
+          variant="transparent"
         />
         <Button
           disabled={!publicKey}
